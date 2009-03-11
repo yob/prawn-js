f24cb15d2ef627be42cad7c26068ba303c05581c
James Healy <jimmy@deefa.com>
2009-03-11 13:58:57 +1100

    new example for will_save JS

---
+++ examples/doc_save.rb
@@ -0,0 +1,13 @@
+$LOAD_PATH.unshift(File.dirname(__FILE__) + '/../lib')
+
+require 'rubygems'
+require 'prawn'
+require 'prawn/js'
+
+Prawn::Document.generate "js_doc_save.pdf" do
+
+  add_will_save_js("alert", "app.alert('saving',3)")
+
+  text "Javascript Prototype", :at => [150,720], :size => 32
+
+end
